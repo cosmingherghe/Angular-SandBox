960487a75883b560ce8ea464e769a96091a57944
Cosmin Gherghe <hello@cosmingherghe.dev>
2023-07-02 21:02:28 -0400

    adding style

--- 02StartNoStrict/src/app/discipline/discipline.component.ts
+++ 02StartNoStrict/src/app/discipline/discipline.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app-discipline",
-    templateUrl: "./discipline.component.html"
+    templateUrl: "./discipline.component.html",
+    styles: ['p {color: goldenrod }']
 })
 export class DisciplineComponent {
 
