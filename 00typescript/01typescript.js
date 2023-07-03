8f638cd8d4bf5564ea49bf4a25aa062cdab0fc91
Cosmin Gherghe <hello@cosmingherghe.dev>
2023-07-02 20:21:45 -0400

    Start

---
+++ 00typescript/01typescript.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b;
+}
+var sum = add(2, 5);
+console.log(sum);
---
+++ 00typescript/01typescript.ts
@@ -0,0 +1,14 @@
+function add(a:number, b:number) {
+    return a + b;
+}
+
+const sum = add(2,5);
+
+console.log(sum);
+
+let hobbies: number[] | string[];
+hobbies = ['Sport', 'Cooking', 'Reading'];
+
+hobbies = [1,2,3];
+
+
