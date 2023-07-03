c5770d1bc8827022f63b68d45bc91594cf8ef509
Cosmin Gherghe <hello@cosmingherghe.dev>
2023-07-03 11:35:37 -0400

    refactoring

--- 02StartNoStrict/src/app/discipline/discipline.component.ts
+++ 02StartNoStrict/src/app/discipline/discipline.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-discipline",
     templateUrl: "./discipline.component.html",
-    styles: ['p {color: goldenrod }']
+    styles: [`p {color: goldenrod }`]
 })
 export class DisciplineComponent {
 
